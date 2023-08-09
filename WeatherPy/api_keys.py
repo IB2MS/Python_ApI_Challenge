b98bcf8f43299f5a946885865cc8f49068cfca10
Olesya Gaeva <137449592+IB2MS@users.noreply.github.com>
2023-08-09 19:09:00 -0400

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "7cdb1dc5d1a7e624cad6ce7f7d445bbe"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "3e16d7f3733c496caae5b57eb777885a"
+geoapify_key = ""
